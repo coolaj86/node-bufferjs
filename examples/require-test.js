89bcd5650c6cf4ee65e20c942b4742c436c65906
AJ ONeal <coolaj86@gmail.com>
2011-02-27 17:16:31 -0700

    making layout the same as in the e-mail I just sent to the npm list

--- examples/require-test.js
+++ examples/require-test.js
@@ -1,5 +1,9 @@
 require('bufferjs');
 require('bufferjs/concat');
 require('bufferjs/add-chunk');
+
+console.log('Buffer.concat:');
 console.log(Buffer.concat);
+
+console.log('buffer.addChunk:');
 console.log((new Buffer(1).addChunk));
