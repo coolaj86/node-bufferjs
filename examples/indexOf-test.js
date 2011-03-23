244e1e5d0be5861e3583d26f794bf162ff53f747
AJ ONeal <coolaj86@gmail.com>
2011-03-22 20:31:58 -0600

    stylized

--- examples/indexOf-test.js
+++ examples/indexOf-test.js
@@ -1,27 +1,33 @@
-require('bufferjs/indexOf');
-var assert = require('assert');
+(function () {
+  "use strict";
 
-// Test the static version, Buffer needle
-function test1() {
-  var haystack = new Buffer(10);
-  var needle = new Buffer(1);
-  needle[0] = haystack[0] = 255;
-  assert.equal(Buffer.indexOf(haystack, needle), 0);
-  assert.equal(Buffer.indexOf(haystack, new Buffer(haystack.length)), -1);
-}
+  require('bufferjs/indexOf');
 
-// Test the OO version, String needle
-function test2() {
-  var haystack = new Buffer(20);
-  var needle = '\r\n';
-  var index  = 5;
-  haystack.write(needle, index);
-  assert.equal(haystack.indexOf(needle), index);
-  assert.equal(haystack.indexOf('should not be found'), -1);
-}
+  var assert = require('assert');
 
+  // Test the static version, Buffer needle
+  function test1() {
+    var haystack = new Buffer(10)
+      , needle = new Buffer(1);
 
-test1();
-test2();
+    needle[0] = haystack[0] = 255;
+    assert.equal(Buffer.indexOf(haystack, needle), 0);
+    assert.equal(Buffer.indexOf(haystack, new Buffer(haystack.length)), -1);
+  }
 
-console.log("Done. `indexOf` tests passed!");
+  // Test the OO version, String needle
+  function test2() {
+    var haystack = new Buffer(20)
+      , needle = '\r\n'
+      , index = 5;
+
+    haystack.write(needle, index);
+    assert.equal(haystack.indexOf(needle), index);
+    assert.equal(haystack.indexOf('should not be found'), -1);
+  }
+
+  test1();
+  test2();
+
+  console.log("Done. `indexOf` tests passed!");
+}());
