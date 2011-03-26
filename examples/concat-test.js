ead539715213e7b698611655f6937185348debac
Nathan Rajlich <nathan@tootallnate.net>
2011-03-25 17:27:47 -0700

    Update concat test to use 'assert' module and test for new String behavior.

--- examples/concat-test.js
+++ examples/concat-test.js
@@ -1,16 +1,19 @@
 (function () {
   "use strict";
 
-  require('../lib/concat');
+  require('../bufferjs/concat');
+  var assert = require('assert');
 
   var abc = new Buffer('abc'),
-    def = new Buffer('def'),
+    def = 'def', // Accepts Strings as well
     ghi = new Buffer('ghi'),
+    realResult = abc + def + ghi,
     data = Buffer.concat(abc, def, ghi); 
 
   console.log(data.toString());
+  assert.equal(abc+def+ghi, data.toString());
 
   data = Buffer.concat([abc, def, ghi]);
   console.log(data.toString());
-  console.log("Passed if both strings were 'abcdefghi'");
+  assert.equal(abc+def+ghi, data.toString());
 }());
