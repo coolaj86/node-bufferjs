feda54bb5b355638593fc0ce29b70ea7931e9123
AJ ONeal <coolaj86@gmail.com>
2011-10-13 21:53:48 -0600

    added license, removed provide(), bumped version

--- bufferjs/index.js
+++ bufferjs/index.js
@@ -1,10 +1,7 @@
-var provide = provide || function () {};
 (function () {
   "use strict";
 
   require('./concat');
   require('./add-chunk');
   require('./indexOf');
-
-  provide('buffer');
 }());
