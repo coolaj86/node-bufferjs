51921807fafe5bd58859413695429487951bca89
justinfreitag <justin.freitag@auspost.com.au>
2012-03-05 11:35:00 +1100

    Added support for nulls and cleaned it up a bit...

--- bufferjs/concat.js
+++ bufferjs/concat.js
@@ -5,9 +5,9 @@
     if (!Array.isArray(bufs)) {
       bufs = Array.prototype.slice.call(arguments);
     }
-    
-    var bufsToConcat = [], length = 0;    
-    bufsToConcat.forEach(function (buf) {
+
+    var bufsToConcat = [], length = 0;
+    bufs.forEach(function (buf) {
       if (buf) {
         if (!Buffer.isBuffer(buf)) {
           buf = new Buffer(buf);
@@ -15,8 +15,8 @@
         length += buf.length;
         bufsToConcat.push(buf);
       }
-    }
-    
+    });
+
     var concatBuf = new Buffer(length), index = 0;
     bufsToConcat.forEach(function (buf) {
       buf.copy(concatBuf, index, 0, buf.length);
